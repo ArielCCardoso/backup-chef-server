49c79ac1d7df385aa564c3a6c308dcc8f9bf9d5e
Ariel C. Cardoso <arielccardoso@live.com>
2018-07-17 17:08:45 -0300

    Ajustes no Cookbook

--- metadata.rb
+++ metadata.rb
@@ -20,3 +20,4 @@ chef_version '>= 12.14' if respond_to?(:chef_version)
 # source_url 'https://github.com/<insert_org_here>/backup-chef-server'
 
 depends 'cron', '~> 6.1.1'
+#depends 'yum-microsoft'
