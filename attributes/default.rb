6945d045c2b283a6d9bd8f4103ad1a11536617fa
Ariel C. Cardoso <arielccardoso@live.com>
2018-07-18 16:47:36 -0300

    Adiciona atributos defaults

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['backup-chef-server']['user'] = 'root'
+default['backup-chef-server']['path'] = '/var/opt/chef-backup'
+default['backup-chef-server']['ext'] = 'tgz'
+default['backup-chef-server']['scripts'] = '/devops/.scripts'
