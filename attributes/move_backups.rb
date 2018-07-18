53776aef6048c7f1763f675810b177450dba1d75
Ariel C. Cardoso <arielccardoso@live.com>
2018-07-18 16:47:20 -0300

    Adiciona atributos do recipe move_backups

---
+++ attributes/move_backups.rb
@@ -0,0 +1,4 @@
+default['backup-chef-server']['diario']['prefix_name']  = 'backup_chef_diario_'
+default['backup-chef-server']['mensal']['prefix_name']  = 'backup_chef_mensal_'
+default['backup-chef-server']['diario']['script_name']  = 'diario.sh'
+default['backup-chef-server']['mensal']['script_name']  = 'mensal.sh'
