1de98ac72a8e4424688882551fd45ae063a15bb8
Ariel C. Cardoso <arielccardoso@live.com>
2018-07-18 15:11:48 -0300

    Ajustes

--- attributes/mount_blob.rb
+++ attributes/mount_blob.rb
@@ -5,6 +5,4 @@ default['backup-chef-server']['mount']['path_temp'] = '/mnt/resource/chef-server
 default['backup-chef-server']['mount']['config'] = '/devops/blob.cfg'
 default['backup-chef-server']['mount']['databag']['name'] = 'bigdata-databag'
 default['backup-chef-server']['mount']['databag']['item'] = 'azure-blob'
-#default['backup-chef-server']['mount']['access']['accountName'] =
-#default['backup-chef-server']['mount']['access']['accessKey'] =
-#default['backup-chef-server']['mount']['access']['containerName'] =
+
